3063c71cf2d9aea77e516dce5ce5016312d97543
kelly_maass <kmaass77627@gmail.com>
2020-10-21 23:28:04 -0500

    fix params for fetch

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,14 +14,9 @@ class UsersController < ApplicationController
             end
         end
     
-    
-     # def show
-    #     user = User.find_by(id: params[:id])
-    # render json: user
-    # end 
         private
 
     def user_params
-        params.require(:user).permit(:name)
+        params.require(:user).permit(:username)
     end
 end
